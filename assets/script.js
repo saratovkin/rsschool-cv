272ff812d3bc463920916789dc30c33f22b5b58a
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-18 18:17:55 +0300

    fix: fix burger menu animation

--- assets/script.js
+++ assets/script.js
@@ -23,31 +23,30 @@ function scrollFunction() {
     }
 }
 
-
 function showMenu() {
     if (flag) {
-        menuBox.style.display = 'block';
-        menuBox.style.animation = 'fadeinsolid .5s forwards';
-        menuBox.style.opacity = '0%';
-
         upButton.style.backgroundColor = '#000';
         openIcon.style.display = 'none';
         closeIcon.style.display = 'block';
-        darkBox.style.display = 'block';
+
+        menuBox.style.animation = 'fadeinsolid .5s forwards';
+        menuBox.style.opacity = '0%';
+
         darkBox.style.animation = 'fadeintransparent .5s forwards'
         darkBox.style.opacity = '0%';
 
         flag = false;
-
     } else {
-        menuBox.style.animation = 'fadeout .5s forwards';
-        menuBox.style.opacity = '100%';
         upButton.style.backgroundColor = '#aaabab';
         openIcon.style.display = 'block';
         closeIcon.style.display = 'none';
     
-        darkBox.style.animation = 'fadeout .5s forwards'
+        menuBox.style.animation = 'fadeoutsolid .5s forwards';
+        menuBox.style.opacity = '100%';
+
+        darkBox.style.animation = 'fadeouttransparent .5s forwards'
         darkBox.style.opacity = '70%';
+
         flag = true;
     }
 }
